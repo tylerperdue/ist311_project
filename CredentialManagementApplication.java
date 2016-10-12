99369209bffdacb3fe94f67b28b7ccd1c08c11ef
tylerperdue <perduetyler1@gmail.com>
2016-10-12 12:00:14 -0400

    First commit

---
+++ CredentialManagementApplication.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class CredentialManagementApplication {
+	public static void main(String[] args)
+	{
+		LoginWindow l = new LoginWindow();
+		l.setVisible(true);
+	}
+}	
---
+++ LoginWindow.java
@@ -0,0 +1,133 @@
+/*
+ * Author: Fahad Abunayyan
+ */
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import javax.swing.JOptionPane;
+import java.util.Arrays;
+
+
+public class LoginWindow extends JFrame implements ActionListener
+{
+
+	JTextField txtUsername = new JTextField(10);
+	JPasswordField txtPassword = new JPasswordField(10);
+	
+	
+	JLabel lblLogin = new JLabel("Please Login:");
+	JLabel lblUsername = new JLabel("Username:");
+	JLabel lblPassword = new JLabel("Password:");
+	
+	JPanel p1 = new JPanel();
+	JPanel p2 = new JPanel();
+	JPanel p3 = new JPanel();
+	JPanel p4 = new JPanel();
+	JPanel p5 = new JPanel();
+
+	
+	JButton btnLogin = new JButton("Login");
+	JButton btnCancel = new JButton("Cancel");
+	JButton btnNewAccount = new JButton("Create New Account");
+
+	
+	
+	public LoginWindow()
+	{
+		
+		this.setLayout(new FlowLayout());
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(250, 340);
+		this.setLocation(500, 250);
+		this.getContentPane().setBackground(Color.WHITE);
+		
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+//		add(new JLabel(new ImageIcon("vaultii.png")));
+	
+		p1.setBackground(Color.WHITE);
+		p1.add(lblUsername);
+		p1.add(txtUsername);
+		
+		p2.setBackground(Color.WHITE);
+		p2.add(lblPassword);
+		p2.add(txtPassword);
+		
+		p3.setBackground(Color.WHITE);
+		p3.add(btnCancel);
+		p3.add(btnLogin);
+
+		p4.add(lblLogin);
+		p4.setBackground(Color.WHITE);
+
+		p5.add(btnNewAccount);
+		p5.setBackground(Color.WHITE);
+
+		
+	
+		add(p4);
+		add(p1);
+		add(p2);
+		add(p3);
+		add(p5);
+
+		
+		this.btnCancel.addActionListener(this);
+		this.btnLogin.addActionListener(this);
+		this.btnNewAccount.addActionListener(this);
+	}
+	
+	
+	public void actionPerformed(ActionEvent ae)
+	{
+		switch(ae.getActionCommand())
+		{
+		
+		case "Cancel":
+			this.dispose();
+			this.setVisible(false);
+		break;
+			
+		case "Login":
+			System.out.println("Login");
+			if(txtUsername.getText().equals("username") && Arrays.equals(txtPassword.getPassword(), new char[]{'p','a','s','s','w','o','r','d'})){
+				JOptionPane.showMessageDialog(null, "Successfully Authenticated!");
+			}else{
+				JOptionPane.showMessageDialog(null, "Username and/or password incorrect.");
+			}
+			//TODO: Add logic to check login information with excel sheet or list
+			break;
+			
+			default:
+				System.err.println("Error");
+
+		case "Create New Account":
+			this.dispose();
+			RegistrationWindow r = new RegistrationWindow();
+			r.setVisible(true);
+		break;			
+		}
+		
+	}
+//	
+//	public static void main(String[] args)
+//	{
+//		
+//		cLogin login = new cLogin();
+//		login.setVisible(true);
+//	}
+//	
+}
