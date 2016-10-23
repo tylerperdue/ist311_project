13da9bd4d5fd86cd086fbe03799b1f034d9cf812
ffa110 <macbookpro@100.65.6.234>
2016-10-22 21:54:12 -0400

    added documentation for RegistrationWindow

--- RegistrationWindow.java
+++ RegistrationWindow.java
@@ -29,41 +29,61 @@ import java.util.Scanner;
 public class RegistrationWindow extends JFrame implements ActionListener
 {
 	
-	JLabel lblIntro = new JLabel("Please enter a new Username and Password:");
+	JLabel lblIntro,			// label to show  message to tell user what to do
+		   lblRepeatPassword,	// label to ask user to re-enter password
+		   lblUsername,			// label to ask user to enter username
+		   lblPassword;			// label to ask user to password
 	
-	JLabel lblUsername = new JLabel("New Username:");
-	JTextField txtUsername = new JTextField(10);
+	JTextField txtUsername;		// text field for user to enter username
 	
-	JLabel lblPassword = new JLabel("New Password:");
-	JPasswordField txtPassword = new JPasswordField(10);
+	JPasswordField txtPassword,		// password text field to enter password and provides a mask for password
+				   txtPasswordAgain;	// text field to prompt user to enter password again
+
+	JButton btnCancel,	// button to allow user to interact with application, cancel the process
+			btnRegister;	// button for user to press after the text fields are filled out
 	
-	JLabel lblRepeatPassword = new JLabel("Re-enter Password:");
-	JPasswordField txtPasswordAgain = new JPasswordField(10);
-	
-	JButton btnCancel = new JButton("Cancel");
-	JButton btnOK = new JButton("OK");
-	
-	JPanel p1 = new JPanel();
-	JPanel p2 = new JPanel();
-	JPanel p3 = new JPanel();
-	JPanel p0 = new JPanel();
+	JPanel p1,
+		   p2,
+		   p3,
+		   p0;
+
 	
 	RegistrationWindow()
 	{
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		lblIntro = new JLabel("Please enter a new Username and Password:");		// label to show  message to tell user what to do
+		lblUsername = new JLabel("New Username:");								// label to ask user to enter username			
+		lblPassword = new JLabel("New Password:");								// label to ask user to password
+		lblRepeatPassword = new JLabel("Re-enter Password:");					// label to ask user to re-enter password
+		
+		txtPassword = new JPasswordField(10);		// text field for password, 10 means the size of the text field
+		txtPasswordAgain = new JPasswordField(10);	// text field for re-entered password, 10 means the size of the text field
+		
+		txtUsername = new JTextField(10);		
+		
+		btnCancel = new JButton("Cancel");
+		btnRegister = new JButton("Register");
+
+		p1 = new JPanel();
+		p2 = new JPanel();
+		p3 = new JPanel();
+		p0 = new JPanel();
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// close the application when user clicks on [X]
 		this.setLocation(400, 200);
 		this.setLayout(new FlowLayout());
 		this.setSize(340, 410);
-		this.setResizable(false);
+		this.setResizable(false);		// false = do not allow user to resize the window
 		
 		lblIntro.setFont(new Font("Tahoma", Font.PLAIN, 16)); // change the font and size 
 		
 //		add(new JLabel(new ImageIcon("cm.png"))); // for adding an image
 		
 		
-		this.getContentPane().setBackground(Color.WHITE);
+		this.getContentPane().setBackground(Color.WHITE);	// change the color of the frame
 		
-		p1.setBackground(Color.WHITE);
+		p1.setBackground(Color.WHITE);						// change the color of the panel
 		p1.add(lblIntro);
 		
 		p2.setLayout(new GridLayout(3,2));
@@ -79,11 +99,15 @@ public class RegistrationWindow extends JFrame implements ActionListener
 		p2.add(lblRepeatPassword);
 		p2.add(txtPasswordAgain);		
 		p3.add(btnCancel);
-		p3.add(btnOK);
+		p3.add(btnRegister);
+		
+
 		
 		
 		this.btnCancel.addActionListener(this);
-		this.btnOK.addActionListener(this);
+		this.btnRegister.addActionListener(this);
+		
+
 		add(p1);
 		add(p2);
 		add(p3);
@@ -106,7 +130,7 @@ public class RegistrationWindow extends JFrame implements ActionListener
 			y.setVisible(true);
 		break;
 		
-		case "OK":
+		case "Register":
 			RegistrationController rc = new RegistrationController(txtUsername.getText(), txtPassword.getPassword(), 
 																   txtPasswordAgain.getPassword());
 			String result = rc.register();
