3e9ee0ea71ed5bc55ef34afb87c1e7db154b86e4
Tyler Perdue <perduetyler1@gmail.com>
2016-11-30 12:03:50 -0500

    Added JOptionPane when user changes password

--- UserAccountWindow.java
+++ UserAccountWindow.java
@@ -104,6 +104,7 @@ public class UserAccountWindow extends JPanel implements ActionListener
                 newPasswordField.getPassword(), reenterNewPasswordField.getPassword());
             String result = submit.update();
             if(result.equals("OK")){
+                JOptionPane.showMessageDialog(null, "Password changed successfully.")
                 MainFrame.mainFrame.getContentPane().removeAll();      
                 NavigationWindow y = new NavigationWindow(loggedUser);
                 MainFrame.mainFrame.add(y);
